afdd24e1d16c8f3036fa5d3f035c59ad0d972fff
malvezzidatr <malvezzi.dev@gmail.com>
2023-10-05 15:25:59 -0300

    Deixando rotas privadas

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -60,6 +60,7 @@ export class UserController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Get('list/email/:email')
   @HttpCode(HttpStatus.OK)
   async getUserByEmail(@Param('email') email: string) {
@@ -74,6 +75,7 @@ export class UserController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Put('update/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateUser(
